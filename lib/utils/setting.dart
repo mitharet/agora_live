8a597d6343426ce4022bb9d1de3c41f827709d8d
Yudha Pratama Putra <yudha.pratama@sinergibest.com>
2021-08-19 09:40:08 +0700

    bypas splash

--- lib/utils/setting.dart
+++ lib/utils/setting.dart
@@ -1,6 +1,4 @@
 // Agora AppId
-// const APP_ID = 'f9f5b112ca6f44cdb368510fdccb9c78';
-
 const APP_ID = 'f9f5b112ca6f44cdb368510fdccb9c78';
 const Token = '006f9f5b112ca6f44cdb368510fdccb9c78IACHACr5Sg+Dk/AmtkfroZgK2jRI2ThT97UNMCNPXraqLJD2TrYAAAAAEAA4tIbZ1qYeYQEAAQDVph5h';
 
